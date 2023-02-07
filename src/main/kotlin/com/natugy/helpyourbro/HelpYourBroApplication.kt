7c85bf02f75b4714f0a6364d97dc2983180856cf
Natugy <tanguy.sergeant@gmail.com>
2023-02-07 20:21:44 +0100

    First Commit

---
+++ src/main/kotlin/com/natugy/helpyourbro/HelpYourBroApplication.kt
@@ -0,0 +1,11 @@
+package com.natugy.helpyourbro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelpYourBroApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelpYourBroApplication>(*args)
+}
---
+++ src/test/kotlin/com/natugy/helpyourbro/HelpYourBroApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.natugy.helpyourbro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelpYourBroApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
